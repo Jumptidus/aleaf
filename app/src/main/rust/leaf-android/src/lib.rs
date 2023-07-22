793125208ec65d23410ffa1b11c753d04cdfc017
snom <snom.hero@gmail.com>
2023-07-22 12:00:14 +0800

    upgrade cargo.lock

--- app/src/main/rust/leaf-android/src/lib.rs
+++ app/src/main/rust/leaf-android/src/lib.rs
@@ -3,33 +3,62 @@ use jni::{
     JNIEnv,
 };
 
+/// No error.
+pub const ERR_OK: i32 = 0;
+/// Config path error.
+pub const ERR_CONFIG_PATH: i32 = 1;
+/// Config parsing error.
+pub const ERR_CONFIG: i32 = 2;
+/// IO error.
+pub const ERR_IO: i32 = 3;
+/// Config file watcher error.
+pub const ERR_WATCHER: i32 = 4;
+/// Async channel send error.
+pub const ERR_ASYNC_CHANNEL_SEND: i32 = 5;
+/// Sync channel receive error.
+pub const ERR_SYNC_CHANNEL_RECV: i32 = 6;
+/// Runtime manager error.
+pub const ERR_RUNTIME_MANAGER: i32 = 7;
+/// No associated config file.
+pub const ERR_NO_CONFIG_FILE: i32 = 8;
+
+fn to_errno(e: leaf::Error) -> i32 {
+    match e {
+        leaf::Error::Config(..) => ERR_CONFIG,
+        leaf::Error::NoConfigFile => ERR_NO_CONFIG_FILE,
+        leaf::Error::Io(..) => ERR_IO,
+        #[cfg(feature = "auto-reload")]
+        leaf::Error::Watcher(..) => ERR_WATCHER,
+        leaf::Error::AsyncChannelSend(..) => ERR_ASYNC_CHANNEL_SEND,
+        leaf::Error::SyncChannelRecv(..) => ERR_SYNC_CHANNEL_RECV,
+        leaf::Error::RuntimeManager => ERR_RUNTIME_MANAGER,
+    }
+}
+
 #[allow(non_snake_case)]
 #[no_mangle]
 pub unsafe extern "C" fn Java_com_noob_NoobVpnService_runLeaf(
     mut env: JNIEnv,
     _: JClass,
+    rt_id: u16,
     config_path: JString,
-    _protect_path: JString,
-) {
-    let config_path = env
-        .get_string(config_path.as_ref())
+) -> i32 {
+    let config_path = env.get_string(config_path.as_ref())
         .unwrap()
         .to_str()
         .unwrap()
         .to_owned();
-    // let protect_path = env
-    //     .get_string(_protect_path)
-    //     .unwrap()
-    //     .to_str()
-    //     .unwrap()
-    //     .to_owned();
+
     let opts = leaf::StartOptions {
-        config: leaf::Config::File(config_path),
-        // socket_protect_path: Some(protect_path),
-        auto_reload: true,
-        runtime_opt: leaf::RuntimeOption::MultiThreadAuto(2),
+        config: leaf::Config::File(config_path.to_string()),
+        #[cfg(feature = "auto-reload")]
+        auto_reload: false,
+        runtime_opt: leaf::RuntimeOption::SingleThread,
     };
-    leaf::start(10086, opts).unwrap();
+    if let Err(e) = leaf::start(rt_id, opts) {
+        return to_errno(e);
+    }
+    ERR_OK
 }
 
 #[allow(non_snake_case)]
@@ -37,8 +66,9 @@ pub unsafe extern "C" fn Java_com_noob_NoobVpnService_runLeaf(
 pub unsafe extern "C" fn Java_com_noob_NoobVpnService_stopLeaf(
     _: JNIEnv,
     _: JClass,
-) {
-    leaf::shutdown(10086);
+    rt_id: u16
+) -> bool {
+    leaf::shutdown(rt_id)
 }
 
 
@@ -47,6 +77,10 @@ pub unsafe extern "C" fn Java_com_noob_NoobVpnService_stopLeaf(
 pub unsafe extern "C" fn Java_com_noob_NoobVpnService_reloadLeaf(
     _: JNIEnv,
     _: JClass,
-) {
-    leaf::reload(10086).unwrap();
+    rt_id: u16
+) -> i32{
+    if let Err(e) = leaf::reload(rt_id){
+        return to_errno(e);
+    }
+    ERR_OK
 }
